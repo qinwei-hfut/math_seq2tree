9df4877de8e204931c437459995ea50b8391221a
willyqin <qinwei206@gmail.com>
2021-04-23 19:57:29 +0800

    update

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -929,7 +929,7 @@ def compute_tree_distance(idx_equation, lang):
     # pdb.set_trace()
     for c in equation:
         stack.push(c)
-    return stack.base[0].dist, equation
+    return (stack.base[0].dist,equation)
     # print(stack.base[0].dist)
     # pdb.set_trace()
 
@@ -951,7 +951,7 @@ def train_probing_distance(input_batch, input_length,output_batch, output_length
     # 没有影响的，因为encoder的数据是每个batch一起来的；
     for idx in range(len(input_batch)):
         # pdb.set_trace()
-        dist_dict, equation = compute_tree_distance(output_batch[idx][0:output_length[idx]],output_lang)
+        dist_dict,equation = compute_tree_distance(output_batch[idx][0:output_length[idx]],output_lang)
         for i in range(len(num_pos[idx])):
             for j in range(i+1,len(num_pos[idx])):
                 pdb.set_trace()
