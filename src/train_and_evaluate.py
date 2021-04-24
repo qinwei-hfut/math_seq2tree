e3c29d70c5049f06c4c0d77c09b1fe6f6b1111c6
willyqin <qinwei206@gmail.com>
2021-04-24 11:55:28 +0800

    debug

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1011,7 +1011,7 @@ def train_probing_distance(input_batch, input_length,output_batch, output_length
                             if kk == Num_list[j] or kk.find(Num_list[j]+'__') != -1:
                                 edges.append(vv)
                 distance_tree = sum(edges) / len(edges)
-                print(Num_list[i]+'   '+Num_list[j]+': '+distance_tree)
+                print(str(Num_list[i])+'   '+str(Num_list[j])+': '+str(distance_tree))
 
                 dist_feautre = probing_distance_module(feature_i,feature_j)
                 loss_pbl.append(torch.abs(dist_feautre - distance_tree))
