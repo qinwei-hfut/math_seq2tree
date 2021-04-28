1c6470648de4b141ab966d60f66cb2608fc9ac6e
willyqin <qinwei206@gmail.com>
2021-04-28 15:23:19 +0800

    update

--- run_probing_task.py
+++ run_probing_task.py
@@ -226,7 +226,7 @@ for fold in range(5):
 
         # pdb.set_trace()
         print("training loss:", loss_total / len(input_lengths))
-        print("training acc:", sum(correct_list_total).item() / len(correct_list_total))
+        print("training acc:", sum(correct_list_total) / len(correct_list_total))
         print("training time", time_since(time.time() - start))
         print("--------------------------------")
         
@@ -314,7 +314,7 @@ for fold in range(5):
         # if  float(correct_total_test)/len(test_pairs) > best_test_acc:
         #     best_test_acc =  float(correct_total_test)/len(test_pairs)
         print("test loss:", sum(loss_total) / len(loss_total))
-        print("test acc:", sum(correct_total_total_test).item() / len(correct_total_total_test))
+        print("test acc:", sum(correct_total_total_test) / len(correct_total_total_test))
         print("test time", time_since(time.time() - start))
         print("--------------------------------")
         
