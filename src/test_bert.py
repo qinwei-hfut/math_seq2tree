54c5f3f4b7ac4ef73595f01846206213eaab6ca1
willyqin <qinwei206@gmail.com>
2021-05-08 20:30:26 +0800

    while

--- src/test_bert.py
+++ src/test_bert.py
@@ -7,12 +7,16 @@ tokenizer = BertTokenizer.from_pretrained(pretrained)
 model = BertModel.from_pretrained(pretrained)
 
 # outputs.last_hidden_state
-input_text = "第一天挖了316方，连续挖了NUM天，一周共挖土，多少方？"
-input_text = "第一天挖，了316方"
-inputs = tokenizer(input_text, return_tensors="pt")
-# inputs.input_ids
-outputs = model(**inputs)
-pooler_output  = outputs.pooler_output
-last_hidden_state   = outputs.last_hidden_state
-pdb.set_trace()
-print()
+while True:
+    input_text = "第一天挖了316方，连续挖了NUM天，一周共挖土，多少方？"
+    input_text = "第一天挖了316方，"
+    pdb.set_trace()
+    inputs = tokenizer(input_text, return_tensors="pt")
+    # inputs.input_ids
+    print(len(input_text))
+    print(inputs.input_ids.shape)
+    outputs = model(**inputs)
+    pooler_output  = outputs.pooler_output
+    last_hidden_state   = outputs.last_hidden_state
+    # pdb.set_trace()
+    print()
