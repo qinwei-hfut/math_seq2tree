dac9661470b72c7c112653bebac02e8af92f56e4
willyqin <qinwei206@gmail.com>
2021-05-08 13:06:16 +0800

    trace

--- src/test_bert.py
+++ src/test_bert.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import BertTokenizer, BertModel
-
+import pdb
 
 pretrained = 'bert-base-chinese'
 tokenizer = BertTokenizer.from_pretrained(pretrained)
@@ -10,5 +10,6 @@ input_text = "少方？"
 inputs = tokenizer(input_text, return_tensors="pt")
 outputs = model(**inputs)
 pooler_output  = outputs.pooler_output
+pdb.set_trace()
 # last_hidden_state   = outputs.last_hidden_state
 print()
