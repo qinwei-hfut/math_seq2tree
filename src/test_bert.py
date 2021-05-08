08d832edad9e784d4933fb440e239f41cf2abf43
willyqin <qinwei206@gmail.com>
2021-05-08 17:07:03 +0800

    trace

--- src/test_bert.py
+++ src/test_bert.py
@@ -6,7 +6,8 @@ pretrained = 'bert-base-chinese'
 tokenizer = BertTokenizer.from_pretrained(pretrained)
 model = BertModel.from_pretrained(pretrained)
 
-input_text = "第一天挖了316方，连续挖了NUM天，一周共挖土多少方？"
+# outputs.last_hidden_state
+input_text = "第一天挖了316方，连续挖了NUM天，一周共挖土，多少方？"
 inputs = tokenizer(input_text, return_tensors="pt")
 outputs = model(**inputs)
 pooler_output  = outputs.pooler_output
