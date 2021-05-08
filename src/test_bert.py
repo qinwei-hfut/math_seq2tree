ca33cf10e41d6d0102feafcafa312d4fc1502279
willyqin <qinwei206@gmail.com>
2021-05-08 20:26:10 +0800

    tokenizer

--- src/test_bert.py
+++ src/test_bert.py
@@ -8,9 +8,11 @@ model = BertModel.from_pretrained(pretrained)
 
 # outputs.last_hidden_state
 input_text = "第一天挖了316方，连续挖了NUM天，一周共挖土，多少方？"
+input_text = "第一天挖了316方"
 inputs = tokenizer(input_text, return_tensors="pt")
+# inputs.input_ids
 outputs = model(**inputs)
 pooler_output  = outputs.pooler_output
+last_hidden_state   = outputs.last_hidden_state
 pdb.set_trace()
-# last_hidden_state   = outputs.last_hidden_state
 print()
