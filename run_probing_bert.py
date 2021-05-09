17f6eabb2eb84a344077acf3ec76f68f4dae8aad
willyqin <qinwei206@gmail.com>
2021-05-09 22:49:45 +0800

    update

--- run_probing_bert.py
+++ run_probing_bert.py
@@ -160,7 +160,7 @@ for fold in range(5):
         # 因此，input_lengths就是标记了每个样本的实际长度；二维的；
         # input_batches, input_lengths, output_batches, output_lengths, nums_batches, num_stack_batches, num_pos_batches, num_size_batches = prepare_train_batch(train_pairs, batch_size)
         input_batches, input_lengths, output_batches, output_lengths, nums_batches, num_pos_batches, num_size_batches =prepare_train_batch_for_bert(pairs_trained,batch_size)
-        pdb.set_trace()
+        # pdb.set_trace()
         print("fold:", fold + 1)
         print("epoch:", epoch + 1)
 
