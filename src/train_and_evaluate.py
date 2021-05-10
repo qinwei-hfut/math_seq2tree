61d5a8dd1550a8b6ea02500d1e38a9629c63ef27
willyqin <qinwei206@gmail.com>
2021-05-10 13:15:30 +0800

    update

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1067,7 +1067,7 @@ def train_probing_type_bert(input_batch, input_length,output_batch, output_lengt
         outputs = bert_model(**inputs)
         pooler_output  = outputs.pooler_output
         last_hidden_state   = outputs.last_hidden_state
-        pdb.set_trace()
+        # pdb.set_trace()
         
         for idx_np in range(len(num_pos[idx])):
             num_p = num_pos[idx][idx_np]+1
@@ -1087,7 +1087,7 @@ def train_probing_type_bert(input_batch, input_length,output_batch, output_lengt
             # input_x = encoder_outputs[num_p][idx].unsqueeze(dim=0)
 
             pred = probing_type_module(input_x)
-            pdb.set_trace()
+            # pdb.set_trace()
             loss_np = criterion(pred,target)
             correct_list_batch.append((torch.max(pred,1)[1]==target).item())
             loss_batch.append(loss_np)
