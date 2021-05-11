dd33969068816eaf4a6904283c62fbfdc98e1360
willyqin <qinwei206@gmail.com>
2021-05-11 10:34:02 +0800

    train_probing_opter_bert

--- run_probing_bert.py
+++ run_probing_bert.py
@@ -218,22 +218,23 @@ for fold in range(5):
             loss_total += loss_dist
             '''
 
-            '''
-            loss_probing_compare, correct_list_batch = train_probing_opter(input_batches[idx], input_lengths[idx], output_batches[idx],output_lengths[idx], encoder, probing_opter_module, probing_opter_optim, nums_batches[idx], num_pos_batches[idx],output_lang)
+            
+            loss_probing_compare, correct_list_batch = train_probing_opter_bert(input_batches[idx], input_lengths[idx], output_batches[idx],output_lengths[idx], probing_type_module, probing_type_optim, nums_batches[idx], num_pos_batches[idx],output_lang)
             loss_total += loss_probing_compare
             correct_list_total += correct_list_batch
-            '''
+            
 
 
             # loss_probing_compare = train_probing_regression(input_batches[idx], input_lengths[idx], output_batches[idx],output_lengths[idx], encoder, probing_regression_module, probing_regression_optim, nums_batches[idx], num_pos_batches[idx],output_lang)
             # # print(loss_probing_compare)
             # loss_total += loss_probing_compare
             
-
+            '''
             loss_probing_compare,correct_list_batch = train_probing_type_bert(input_batches[idx], input_lengths[idx], output_batches[idx],output_lengths[idx], probing_type_module, probing_type_optim, nums_batches[idx], num_pos_batches[idx],output_lang)
             # print(loss_probing_compare)
             loss_total += loss_probing_compare
             correct_list_total += correct_list_batch
+            '''
         
 
         # pdb.set_trace()
@@ -307,6 +308,7 @@ for fold in range(5):
         print("--------------------------------")
         '''
 
+        '''
         ##### evaluate probing type
         print('evaluate probing type:')
         start = time.time()
@@ -341,6 +343,7 @@ for fold in range(5):
         print("test acc:", sum(correct_total_total_test) / len(correct_total_total_test))
         print("test time", time_since(time.time() - start))
         print("--------------------------------")
+        '''
         
         
 
