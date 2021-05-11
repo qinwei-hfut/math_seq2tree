c2dcb873b844352fb026190058bb82a1344b6e43
willyqin <qinwei206@gmail.com>
2021-05-11 09:32:39 +0800

    update

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1072,9 +1072,9 @@ def train_probing_type_bert(input_batch, input_length,output_batch, output_lengt
         tokens = tokenizer.decode(input_ids)
         outputs = bert_model(**inputs)
         pooler_output  = outputs.pooler_output
-        # last_hidden_state   = outputs.last_hidden_state.cuda()
+        last_hidden_state   = outputs.last_hidden_state
         # print("test time", time_since(time.time() - start))
-        pdb.set_trace()
+        # pdb.set_trace()
         
         for idx_np in range(len(num_pos[idx])):
             num_p = num_pos[idx][idx_np]+1
