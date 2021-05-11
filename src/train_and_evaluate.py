860b0cd0af37c511ff0726dfcf9683ee9745bb06
willyqin <qinwei206@gmail.com>
2021-05-11 09:25:37 +0800

    time and trace

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -1063,13 +1063,15 @@ def train_probing_type_bert(input_batch, input_length,output_batch, output_lengt
     criterion = torch.nn.CrossEntropyLoss()
     for idx in range(len(input_batch)):
 
+        start = time.time()
         inputs = tokenizer(input_batch[idx], return_tensors="pt")
         input_ids = tokenizer.encode(input_batch[idx])
         tokens = tokenizer.decode(input_ids)
         outputs = bert_model(**inputs)
         pooler_output  = outputs.pooler_output
         last_hidden_state   = outputs.last_hidden_state.cuda()
-        # pdb.set_trace()
+        print("test time", time_since(time.time() - start))
+        pdb.set_trace()
         
         for idx_np in range(len(num_pos[idx])):
             num_p = num_pos[idx][idx_np]+1
